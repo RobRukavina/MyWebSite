588a958649f976101129bfc2ddfc4c2a4a13cfb6
RobRukavina <rehabrob440@gmail.com>
2020-09-14 14:06:48 -0600

    removed phone number and email to avoid spam

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,6 @@ const Contact = () => {
   return (
     <div>
       <h1>Robert Rukavina</h1>
-      <h2>801-896-3738</h2>
-      <h3>robrukavinadev@gmail.com</h3>
       <li>
         LinkedIn:{" "}
         <a href="https://www.linkedin.com/in/robert-rukavina-9bb219187/">
