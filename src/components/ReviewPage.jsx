58cda83f5853cce6c985bf3a025aa8d65a657518
RobRukavina <rehabrob440@gmail.com>
2020-10-08 13:19:24 -0600

    added a PageBody to ReviewPage

--- src/components/ReviewPage.jsx
+++ src/components/ReviewPage.jsx
@@ -4,7 +4,9 @@ class ReviewPage extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div>Leave Me a Review on this page eventually</div>
+        <div className="PageBody">
+          <div>Leave Me a Review on this page eventually</div>
+        </div>
       </React.Fragment>
     );
   }
